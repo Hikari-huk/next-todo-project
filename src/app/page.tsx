29c12ead417929e25c794a247af931d53f3869b5
Hikari <ghikariao974@gmail.com>
2024-08-31 11:14:46 +0900

    default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
-import TodoList from "./components/TodoList";
 
 
 export default function Home() {
   return (
     <main className="container mx-auto p-8 text-center max-w-2xl">
       <h1 className="text-2xl mb-4">Todo App</h1>
-      <TodoList/>
+      <p>TODOアプリを作るためのリポジトリ</p>
     </main>
   );
 }
